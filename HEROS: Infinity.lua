92a8da34673833b79bf92c67f7c6c4b7565fab3c
BrunoLuaUH12 <92273855+BrunoLuaUH12@users.noreply.github.com>
2021-10-11 12:30:44 -0300

    Create HEROS: Infinity.lua

---
+++ HEROS: Infinity.lua
@@ -0,0 +1,268 @@
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local FS76KJHSKDFJS54465 = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local ScrollingFrame = Instance.new("ScrollingFrame")
+local wanda2 = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local captain1 = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local thorragnarok = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local war = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local classicthor = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local remover = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local KillGui = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_8 = Instance.new("UICorner")
+
+--Properties:
+
+FS76KJHSKDFJS54465.Name = "FS76KJHSKDFJS54465"
+FS76KJHSKDFJS54465.Parent = game.CoreGui
+
+Frame.Parent = FS76KJHSKDFJS54465
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BackgroundTransparency = 0.550
+Frame.Position = UDim2.new(0.0763178617, 0, 0.284662575, 0)
+Frame.Size = UDim2.new(0, 284, 0, 351)
+
+ScrollingFrame.Parent = Frame
+ScrollingFrame.Active = true
+ScrollingFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ScrollingFrame.BackgroundTransparency = 1.000
+ScrollingFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ScrollingFrame.Position = UDim2.new(0.0985401869, 0, 0.122571632, 0)
+ScrollingFrame.Size = UDim2.new(0, 221, 0, 307)
+
+wanda2.Name = "wanda2"
+wanda2.Parent = ScrollingFrame
+wanda2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+wanda2.BackgroundTransparency = 0.650
+wanda2.Position = UDim2.new(0.109175816, 0, 0.0155936033, 0)
+wanda2.Size = UDim2.new(0, 83, 0, 42)
+wanda2.Font = Enum.Font.SciFi
+wanda2.Text = "Wanda "
+wanda2.TextColor3 = Color3.fromRGB(255, 255, 255)
+wanda2.TextScaled = true
+wanda2.TextSize = 14.000
+wanda2.TextWrapped = true
+
+UICorner.Parent = wanda2
+
+captain1.Name = "captain1"
+captain1.Parent = ScrollingFrame
+captain1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+captain1.BackgroundTransparency = 0.650
+captain1.Position = UDim2.new(0.538399577, 0, 0.104757793, 0)
+captain1.Size = UDim2.new(0, 83, 0, 42)
+captain1.Font = Enum.Font.SciFi
+captain1.Text = "Capitao America"
+captain1.TextColor3 = Color3.fromRGB(255, 255, 255)
+captain1.TextScaled = true
+captain1.TextSize = 14.000
+captain1.TextWrapped = true
+
+UICorner_2.Parent = captain1
+
+thorragnarok.Name = "thorragnarok"
+thorragnarok.Parent = ScrollingFrame
+thorragnarok.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+thorragnarok.BackgroundTransparency = 0.650
+thorragnarok.Position = UDim2.new(0.109175816, 0, 0.104757793, 0)
+thorragnarok.Size = UDim2.new(0, 83, 0, 42)
+thorragnarok.Font = Enum.Font.SciFi
+thorragnarok.Text = "Thor Ragnarok"
+thorragnarok.TextColor3 = Color3.fromRGB(255, 255, 255)
+thorragnarok.TextScaled = true
+thorragnarok.TextSize = 14.000
+thorragnarok.TextWrapped = true
+
+UICorner_3.Parent = thorragnarok
+
+war.Name = "war"
+war.Parent = ScrollingFrame
+war.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+war.BackgroundTransparency = 0.650
+war.Position = UDim2.new(0.10853532, 0, 0.279126346, 0)
+war.Size = UDim2.new(0, 83, 0, 42)
+war.Font = Enum.Font.SciFi
+war.Text = "Maquina de Combate"
+war.TextColor3 = Color3.fromRGB(255, 255, 255)
+war.TextScaled = true
+war.TextSize = 14.000
+war.TextWrapped = true
+
+UICorner_4.Parent = war
+
+classicthor.Name = "classicthor"
+classicthor.Parent = ScrollingFrame
+classicthor.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+classicthor.BackgroundTransparency = 0.650
+classicthor.Position = UDim2.new(0.109175816, 0, 0.193656281, 0)
+classicthor.Size = UDim2.new(0, 83, 0, 42)
+classicthor.Font = Enum.Font.SciFi
+classicthor.Text = "Thor classico"
+classicthor.TextColor3 = Color3.fromRGB(255, 255, 255)
+classicthor.TextScaled = true
+classicthor.TextSize = 14.000
+classicthor.TextWrapped = true
+
+UICorner_5.Parent = classicthor
+
+remover.Name = "remover"
+remover.Parent = ScrollingFrame
+remover.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+remover.BackgroundTransparency = 0.650
+remover.Position = UDim2.new(0.534515202, 0, 0.193656281, 0)
+remover.Size = UDim2.new(0, 83, 0, 42)
+remover.Font = Enum.Font.SciFi
+remover.Text = "Remover"
+remover.TextColor3 = Color3.fromRGB(255, 255, 255)
+remover.TextScaled = true
+remover.TextSize = 14.000
+remover.TextWrapped = true
+
+UICorner_6.Parent = remover
+
+KillGui.Name = "KillGui"
+KillGui.Parent = ScrollingFrame
+KillGui.BackgroundColor3 = Color3.fromRGB(115, 0, 0)
+KillGui.Position = UDim2.new(0.628959298, 0, 0.0154792722, 0)
+KillGui.Size = UDim2.new(0, 62, 0, 42)
+KillGui.Font = Enum.Font.SourceSans
+KillGui.Text = "X"
+KillGui.TextColor3 = Color3.fromRGB(0, 0, 0)
+KillGui.TextScaled = true
+KillGui.TextSize = 14.000
+KillGui.TextWrapped = true
+
+UICorner_7.Parent = KillGui
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.Position = UDim2.new(0.0457746461, 0, 0, 0)
+TextLabel.Size = UDim2.new(0, 257, 0, 54)
+TextLabel.Font = Enum.Font.SciFi
+TextLabel.Text = "HEROES: Infinite"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+UICorner_8.CornerRadius = UDim.new(0, 20)
+UICorner_8.Parent = Frame
+
+-- Scripts:
+
+local function PNDOKAQ_fake_script() -- Frame.LocalScript 
+	local script = Instance.new('LocalScript', Frame)
+
+	local UserInputService = game:GetService("UserInputService")
+	
+	local gui = script.Parent
+	
+	local dragging
+	local dragInput
+	local dragStart
+	local startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+	end
+	
+	gui.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = gui.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	gui.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+	
+	UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+	end)
+end
+coroutine.wrap(PNDOKAQ_fake_script)()
+local function VOWF_fake_script() -- FS76KJHSKDFJS54465.LocalScript 
+	local script = Instance.new('LocalScript', FS76KJHSKDFJS54465)
+
+	script.Parent.Frame.ScrollingFrame.KillGui.MouseButton1Click:Connect(function()
+		game.CoreGui.FS76KJHSKDFJS54465:Destroy()
+	end)
+	
+	script.Parent.Frame.ScrollingFrame.remover.MouseButton1Click:Connect(function()
+		game:GetService("ReplicatedStorage").RemorphEvent:FireServer()
+	end)
+	
+	script.Parent.Frame.ScrollingFrame.war.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = "IronMan",
+			[2] = "WarMachineMark3"
+		}
+	
+		game:GetService("ReplicatedStorage").MorphEvent:FireServer(unpack(args))
+	end)
+	
+	script.Parent.Frame.ScrollingFrame.thorragnarok.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = "Thor",
+			[2] = "Costume1"
+		}
+	
+		game:GetService("ReplicatedStorage").MorphEvent:FireServer(unpack(args))
+	end)
+	
+	script.Parent.Frame.ScrollingFrame.classicthor.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = "Thor",
+			[2] = "Costume2"
+		}
+	
+		game:GetService("ReplicatedStorage").MorphEvent:FireServer(unpack(args))
+	end)
+	
+	script.Parent.Frame.ScrollingFrame.captain1.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = "CaptainAmerica",
+			[2] = "Costume1"
+		}
+	
+		game:GetService("ReplicatedStorage").MorphEvent:FireServer(unpack(args))
+	end)
+	
+	script.Parent.Frame.ScrollingFrame.wanda2.MouseButton1Click:Connect(function()
+		local args = {
+			[1] = "ScarletWitch",
+			[2] = "Costume2"
+		}
+	
+		game:GetService("ReplicatedStorage").MorphEvent:FireServer(unpack(args))
+	end)
+	
+	
+end
+coroutine.wrap(VOWF_fake_script)()
