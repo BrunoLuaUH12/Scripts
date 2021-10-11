3c3e458dbbc045425a513cb958d1852e6abb94b4
BrunoLuaUH12 <92273855+BrunoLuaUH12@users.noreply.github.com>
2021-10-11 18:02:30 -0300

    Update Iron Man: Battlegrounds.lua

--- Iron Man: Battlegrounds.lua
+++ Iron Man: Battlegrounds.lua
@@ -3,8 +3,8 @@
 
 -- Instances:
 
-local jhghfhghj = Instance.new("ScreenGui")
-local fundo = Instance.new("Frame")
+local gfdgdfgdfgdf = Instance.new("ScreenGui")
+local Fundo = Instance.new("Frame")
 local Frame = Instance.new("Frame")
 local ScrollingFrame = Instance.new("ScrollingFrame")
 local mark47 = Instance.new("TextButton")
@@ -16,43 +16,46 @@ local UICorner_3 = Instance.new("UICorner")
 local endosym = Instance.new("TextButton")
 local UICorner_4 = Instance.new("UICorner")
 local title = Instance.new("TextLabel")
-local obs = Instance.new("TextLabel")
-local close = Instance.new("TextButton")
 local UICorner_5 = Instance.new("UICorner")
+local obs = Instance.new("TextLabel")
 local UICorner_6 = Instance.new("UICorner")
+local close = Instance.new("TextButton")
 local UICorner_7 = Instance.new("UICorner")
+local UICorner_8 = Instance.new("UICorner")
+local UICorner_9 = Instance.new("UICorner")
 
 --Properties:
 
-jhghfhghj.Name = "jhghfhghj"
-jhghfhghj.Parent = game.CoreGui
+gfdgdfgdfgdf.Name = "gfdgdfgdfgdf"
+gfdgdfgdfgdf.Parent = game.CoreGui
 
-fundo.Name = "fundo"
-fundo.Parent = jhghfhghj
-fundo.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-fundo.BackgroundTransparency = 0.440
-fundo.Position = UDim2.new(0.536692858, 0, 0.0551998019, 0)
-fundo.Size = UDim2.new(0, 338, 0, 400)
+Fundo.Name = "Fundo"
+Fundo.Parent = gfdgdfgdfgdf
+Fundo.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Fundo.BackgroundTransparency = 0.550
+Fundo.Position = UDim2.new(0.378442109, 0, 0.19263804, 0)
+Fundo.Size = UDim2.new(0, 380, 0, 494)
 
-Frame.Parent = fundo
+Frame.Parent = Fundo
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BackgroundTransparency = 0.550
-Frame.Position = UDim2.new(0.0925119594, 0, 0.0631595254, 0)
-Frame.Size = UDim2.new(0, 274, 0, 348)
+Frame.BackgroundTransparency = 0.700
+Frame.Position = UDim2.new(0.0531963147, 0, 0.0472509116, 0)
+Frame.Size = UDim2.new(0, 339, 0, 453)
 
 ScrollingFrame.Parent = Frame
 ScrollingFrame.Active = true
-ScrollingFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ScrollingFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 ScrollingFrame.BackgroundTransparency = 1.000
-ScrollingFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ScrollingFrame.Position = UDim2.new(0.155702338, 0, 0.275262177, 0)
-ScrollingFrame.Size = UDim2.new(0, 175, 0, 167)
+ScrollingFrame.BorderSizePixel = 0
+ScrollingFrame.Position = UDim2.new(0.137533635, 0, 0.146195889, 0)
+ScrollingFrame.Size = UDim2.new(0, 245, 0, 314)
+ScrollingFrame.CanvasSize = UDim2.new(0, 0, 0.899999976, 0)
 
 mark47.Name = "mark47"
 mark47.Parent = ScrollingFrame
 mark47.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-mark47.Position = UDim2.new(0.114285544, 0, 0.0197568387, 0)
-mark47.Size = UDim2.new(0, 134, 0, 65)
+mark47.Position = UDim2.new(0.128982887, 0, 0.658227205, 0)
+mark47.Size = UDim2.new(0, 180, 0, 53)
 mark47.Font = Enum.Font.SciFi
 mark47.Text = "Mark 47"
 mark47.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -65,10 +68,10 @@ UICorner.Parent = mark47
 patriot.Name = "patriot"
 patriot.Parent = ScrollingFrame
 patriot.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-patriot.Position = UDim2.new(0.119999826, 0, 0.246200606, 0)
-patriot.Size = UDim2.new(0, 134, 0, 65)
+patriot.Position = UDim2.new(0.133064523, 0, 0.478397965, 0)
+patriot.Size = UDim2.new(0, 180, 0, 53)
 patriot.Font = Enum.Font.SciFi
-patriot.Text = "Patriota de Ferro"
+patriot.Text = "Iron Patriot"
 patriot.TextColor3 = Color3.fromRGB(0, 0, 0)
 patriot.TextScaled = true
 patriot.TextSize = 14.000
@@ -79,10 +82,10 @@ UICorner_2.Parent = patriot
 scavver.Name = "scavver"
 scavver.Parent = ScrollingFrame
 scavver.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-scavver.Position = UDim2.new(0.114285544, 0, 0.46960488, 0)
-scavver.Size = UDim2.new(0, 134, 0, 65)
+scavver.Position = UDim2.new(0.133064523, 0, 0.294669986, 0)
+scavver.Size = UDim2.new(0, 180, 0, 53)
 scavver.Font = Enum.Font.SciFi
-scavver.Text = "Scavver"
+scavver.Text = "Scravver"
 scavver.TextColor3 = Color3.fromRGB(0, 0, 0)
 scavver.TextScaled = true
 scavver.TextSize = 14.000
@@ -93,8 +96,8 @@ UICorner_3.Parent = scavver
 endosym.Name = "endosym"
 endosym.Parent = ScrollingFrame
 endosym.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-endosym.Position = UDim2.new(0.114285544, 0, 0.709726453, 0)
-endosym.Size = UDim2.new(0, 134, 0, 65)
+endosym.Position = UDim2.new(0.133064523, 0, 0.110528536, 0)
+endosym.Size = UDim2.new(0, 180, 0, 53)
 endosym.Font = Enum.Font.SciFi
 endosym.Text = "Endosym"
 endosym.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -108,7 +111,7 @@ title.Name = "title"
 title.Parent = Frame
 title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 title.BackgroundTransparency = 1.000
-title.Position = UDim2.new(0.106673047, 0, 0.00983606558, 0)
+title.Position = UDim2.new(0.200653896, 0, 0.0158727653, 0)
 title.Size = UDim2.new(0, 200, 0, 50)
 title.Font = Enum.Font.SciFi
 title.Text = "Iron Man: Battlegrounds"
@@ -117,11 +120,13 @@ title.TextScaled = true
 title.TextSize = 14.000
 title.TextWrapped = true
 
+UICorner_5.Parent = title
+
 obs.Name = "obs"
 obs.Parent = Frame
 obs.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 obs.BackgroundTransparency = 1.000
-obs.Position = UDim2.new(0.106626727, 0, 0.826947033, 0)
+obs.Position = UDim2.new(0.200507775, 0, 0.855586588, 0)
 obs.Size = UDim2.new(0, 200, 0, 50)
 obs.Font = Enum.Font.SciFi
 obs.Text = "Obs: Quando apertar o botao clique na tecla Q."
@@ -130,29 +135,33 @@ obs.TextScaled = true
 obs.TextSize = 14.000
 obs.TextWrapped = true
 
+UICorner_6.Parent = obs
+
 close.Name = "close"
 close.Parent = Frame
-close.BackgroundColor3 = Color3.fromRGB(131, 0, 0)
-close.Position = UDim2.new(0.759124041, 0, 0.0143678822, 0)
-close.Size = UDim2.new(0, 59, 0, 45)
-close.Font = Enum.Font.SourceSans
+close.BackgroundColor3 = Color3.fromRGB(140, 0, 0)
+close.Position = UDim2.new(0.764674723, 0, 0.00890264474, 0)
+close.Size = UDim2.new(0, 69, 0, 47)
+close.Font = Enum.Font.Roboto
 close.Text = "X"
 close.TextColor3 = Color3.fromRGB(0, 0, 0)
 close.TextScaled = true
 close.TextSize = 14.000
 close.TextWrapped = true
 
-UICorner_5.Parent = close
+UICorner_7.CornerRadius = UDim.new(0, 15)
+UICorner_7.Parent = close
 
-UICorner_6.Parent = Frame
+UICorner_8.CornerRadius = UDim.new(0, 20)
+UICorner_8.Parent = Frame
 
-UICorner_7.CornerRadius = UDim.new(0, 50)
-UICorner_7.Parent = fundo
+UICorner_9.CornerRadius = UDim.new(0, 50)
+UICorner_9.Parent = Fundo
 
 -- Scripts:
 
-local function FKHEV_fake_script() -- fundo.Drag 
-	local script = Instance.new('LocalScript', fundo)
+local function TYBOHPO_fake_script() -- Fundo.Drag 
+	local script = Instance.new('LocalScript', Fundo)
 
 	local UserInputService = game:GetService("UserInputService")
 	
@@ -194,19 +203,19 @@ local function FKHEV_fake_script() -- fundo.Drag
 		end
 	end)
 end
-coroutine.wrap(FKHEV_fake_script)()
-local function FXZPWC_fake_script() -- jhghfhghj.Scripts 
-	local script = Instance.new('LocalScript', jhghfhghj)
+coroutine.wrap(TYBOHPO_fake_script)()
+local function SPAHFBE_fake_script() -- gfdgdfgdfgdf.Script 
+	local script = Instance.new('LocalScript', gfdgdfgdfgdf)
 
-	--Close
+	-- Close
 	
-	script.Parent.fundo.Frame.close.MouseButton1Click:Connect(function()
-		game.CoreGui.jhghfhghj:Destroy()
+	script.Parent.Fundo.Frame.close.MouseButton1Click:Connect(function()
+		game.CoreGui.gfdgdfgdfgdf:Destroy()
 	end)
 	
 	-- Scripts
 	
-	script.Parent.fundo.Frame.ScrollingFrame.endosym.MouseButton1Click:Connect(function()
+	script.Parent.Fundo.Frame.ScrollingFrame.endosym.MouseButton1Click:Connect(function()
 		local args = {
 			[1] = "Endosym"
 		}
@@ -214,15 +223,15 @@ local function FXZPWC_fake_script() -- jhghfhghj.Scripts
 		game:GetService("ReplicatedStorage").Events.callSuit:InvokeServer(unpack(args))
 	end)
 	
-	script.Parent.fundo.Frame.ScrollingFrame.mark47.MouseButton1Click:Connect(function()
+	script.Parent.Fundo.Frame.ScrollingFrame.scavver.MouseButton1Click:Connect(function()
 		local args = {
-			[1] = "Mark 47"
+			[1] = "Scavver"
 		}
 	
-		game:GetService("ReplicatedStorage").Events.callSuit:InvokeServer(unpack(args))	
+		game:GetService("ReplicatedStorage").Events.callSuit:InvokeServer(unpack(args))
 	end)
 	
-	script.Parent.fundo.Frame.ScrollingFrame.patriot.MouseButton1Click:Connect(function()
+	script.Parent.Fundo.Frame.ScrollingFrame.patriot.MouseButton1Click:Connect(function()
 		local args = {
 			[1] = "Iron Patriot"
 		}
@@ -230,12 +239,13 @@ local function FXZPWC_fake_script() -- jhghfhghj.Scripts
 		game:GetService("ReplicatedStorage").Events.callSuit:InvokeServer(unpack(args))
 	end)
 	
-	script.Parent.fundo.Frame.ScrollingFrame.scavver.MouseButton1Click:Connect(function()
+	script.Parent.Fundo.Frame.ScrollingFrame.mark47.MouseButton1Click:Connect(function()
 		local args = {
-			[1] = "Scavver"
+			[1] = "Mark 47"
 		}
 	
-		game:GetService("ReplicatedStorage").Events.callSuit:InvokeServer(unpack(args))
+		game:GetService("ReplicatedStorage").Events.callSuit:InvokeServer(unpack(args))	
 	end)
+	
 end
-coroutine.wrap(FXZPWC_fake_script)()
+coroutine.wrap(SPAHFBE_fake_script)()
